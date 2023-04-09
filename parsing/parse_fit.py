c2461d35f079285535febc64cc3f270159fa4a1c
ryobg <ryobg@users.noreply.github.com>
2023-04-09 19:40:33 +0300

    Refactor

--- parsing/parse_fit.py
+++ parsing/parse_fit.py
@@ -1,14 +1,61 @@
-import math, pytz, csv, os, json
+#!/usr/bin/env python
+
+'''
+@see https://en.wikipedia.org/wiki/Heart_rate_variability
+A Real-Time Automated Point Process Method for Detection and Correction of Erroneous and Ectopic Heartbeats
+@see https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3523127/
+'''
+
+import pytz, csv, os, json
 from tzlocal import get_localzone
 from garmin_fit_sdk import Decoder, Stream
+from traceback import TracebackException
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 EMPTY_REC = 65535
 
 def main ():
-    for f in [f for f in os.listdir () if f[-4:].lower () == '.fit']:
-        process_file (f, f[:-4] + '.csv')
+    for source_file in [f for f in os.listdir () if f[-4:].lower () == '.fit']:
 
-def rr_records_size (records):
+        print ("Processing " + source_file)
+
+        messages, errors = Decoder (Stream.from_file (source_file)).read ()
+        if errors:
+            print ("Decoding error: " + str (errors))
+            continue
+
+        # Store a full, readable copy of the parsed FIT data.
+        with open (source_file[:-4] + '.json', 'w') as f:
+            f.writelines (json.dumps (messages, indent = 4, default = str))
+
+        size = ibi_records_size (messages['record_mesgs'])
+        print ("Amount of records: " + str (size))
+        if size < 1:
+            continue
+
+        try:
+            records = extract_records (messages['record_mesgs'])
+            write_to_csv (source_file[:-4] + '.csv', records)
+        except Exception as e:
+            print ("Error: " + "".join (TracebackException.from_exception (e).format ()))
+            continue
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def write_to_csv (destination_file, records):
+    with open (destination_file, 'w') as f:
+        csvfile = csv.writer (f)
+        csvfile.writerow ([str (k) for k in records.keys ()])
+        for i, _ in enumerate (records.values ()):
+            row = []
+            for k in records.keys ():
+                row.append (str (records[k][i]))
+            csvfile.writerow (row)
+
+#-----------------------------------------------------------------------------------------------------------------------
+
+def ibi_records_size (records):
     i = 0
     for r in records:
         fields = r['developer_fields']
@@ -22,68 +69,47 @@ def rr_records_size (records):
                 i = i + 1
     return i
 
-def process_file (source_file, destination_file):
+#-----------------------------------------------------------------------------------------------------------------------
 
-    messages, errors = Decoder (Stream.from_file (source_file)).read ()
+def extract_records (record_msgs):
+    '''
+    @return dict of lists per timestamp, interbeat_intervals and heart_rate
+    '''
 
-    if errors:
-        print ("%s: Error decoding %s" % (source_file, str (errors)))
-        return
+    # Actually, only the IBI is what is interesting
+    records = {"interbeat_intervals":[], "timestamp":[], "heart_rate":[]}
 
-    with open (source_file[:-4] + '.json', 'w') as f:
-        f.writelines (json.dumps (messages, indent = 4, default = str))
-
-    size = rr_records_size (messages['record_mesgs'])
-    print ("%s: has %d records" % (source_file, size))
-    if size < 1:
-        return
-
-    records = dict ()
-    for msg in messages['record_mesgs']:
-        for k, v in msg.items ():
-            records[k] = []
-
-    records['beat_intervals'] = []
-    for msg in messages['record_mesgs']:
+    for msg in record_msgs:
         fields = msg['developer_fields']
-        rr = [r for r in fields[0] if not r == EMPTY_REC]
+        ibi = [r for r in fields[0] if not r == EMPTY_REC]
         for k, v in msg.items ():
-            if k != 'developer_fields':
-                for i in range (0, len (rr)):
-                    records[k].append (v)
-            else:
-                records['beat_intervals'] += rr
-    for k in [k for k in records.keys () if k not in {"beat_intervals", "timestamp", "heart_rate"}]:
-        records.pop (k)
+            if k == 'developer_fields':
+                records['interbeat_intervals'] += ibi
+            elif k in records:
+                records[k] += [v] * len (ibi)
 
     if len (set ([len (v) for v in records.values ()])) != 1:
-        print ("%s: with header %s" % (source_file, str ([k for k in records.keys ()])))
-        print ("Different amount of records! " + str ([len (v) for v in records.values ()]))
-        return
+        raise Exception ("Header {} amount of records mismatched {}".format (
+            [k for k in records.keys ()], 
+            [len (v) for v in records.values ()]))
 
     # Validation
     duration_a = (max (records['timestamp']) - min (records['timestamp'])).total_seconds ()
-    duration_b = sum (records['beat_intervals']) / 1000
-    print ("%s: Logged %.1f seconds of %.1f beats" % (source_file, duration_a, duration_b))
+    duration_b = sum (records['interbeat_intervals']) / 1000
+    print ("Activity duration: %.1fs, Interval beats duration: %.1fs" % (duration_a, duration_b))
 
-    # Write to CSV file
+    # Type conversions (aka formatting in Spreadsheet applications)
     local_tz = get_localzone ()
-    with open (destination_file, 'w') as f:
-        csvfile = csv.writer (f)
-        csvfile.writerow ([str (k) for k in records.keys ()])
-        for i in range (0, size):
-            row = []
-            for k in records.keys ():
-                v = None
-                if k == 'timestamp':
-                    v = records[k][i].replace (tzinfo=pytz.utc).astimezone (local_tz)
-                # Convert to int so Spreadsheets can recognize for sure
-                elif k == 'beat_intervals' or k == 'heart_rate':
-                    v = int (records[k][i])
-                else:
-                    v = records[k][i]
-                row.append (str (v))
-            csvfile.writerow (row)
+    for i in range (0, len (records['interbeat_intervals'])):
+        for k in records.keys ():
+            if k == 'timestamp':
+                records[k][i] = records[k][i].replace (tzinfo=pytz.utc).astimezone (local_tz)
+            elif k == 'interbeat_intervals' or k == 'heart_rate':
+                records[k][i] = int (records[k][i])
+
+    return records
+
+#-----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
     main ()
