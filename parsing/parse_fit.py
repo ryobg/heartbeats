358fbad679bf9074588ad128cc36d2d15fb8c0b3
ryobg <ryobg@users.noreply.github.com>
2023-04-11 22:47:02 +0300

    Extend the CSV export with more data for analysis.

--- parsing/parse_fit.py
+++ parsing/parse_fit.py
@@ -2,11 +2,11 @@
 
 '''
 @see https://en.wikipedia.org/wiki/Heart_rate_variability
-A Real-Time Automated Point Process Method for Detection and Correction of Erroneous and Ectopic Heartbeats
-@see https://www.ncbi.nlm.nih.gov/pmc/articles/PMC3523127/
+A robust algorithm for heart rate variability time series artefact correction using novel beat classification
+@see https://www.tandfonline.com/doi/abs/10.1080/03091902.2019.1640306 
 '''
 
-import pytz, csv, os, json
+import pytz, csv, os, json, numpy, shutil, systole
 from tzlocal import get_localzone
 from garmin_fit_sdk import Decoder, Stream
 from traceback import TracebackException
@@ -36,7 +36,10 @@ def main ():
 
         try:
             records = extract_records (messages['record_mesgs'])
-            write_to_csv (source_file[:-4] + '.csv', records)
+            records = correct_intervals (records)
+            target_file = source_file[:-4] + '.csv'
+            write_to_csv (target_file, records)
+            shutil.copy (target_file, "current.csv")
         except Exception as e:
             print ("Error: " + "".join (TracebackException.from_exception (e).format ()))
             continue
@@ -45,12 +48,15 @@ def main ():
 
 def write_to_csv (destination_file, records):
     with open (destination_file, 'w') as f:
-        csvfile = csv.writer (f)
+        csvfile = csv.writer (f, lineterminator = '\n')
         csvfile.writerow ([str (k) for k in records.keys ()])
-        for i, _ in enumerate (records.values ()):
+        for i, _ in enumerate (list (records.values ())[0]):
             row = []
             for k in records.keys ():
-                row.append (str (records[k][i]))
+                if i < len (records[k]):
+                    row.append (str (records[k][i]))
+                else:
+                    row.append ("")
             csvfile.writerow (row)
 
 #-----------------------------------------------------------------------------------------------------------------------
@@ -71,13 +77,46 @@ def ibi_records_size (records):
 
 #-----------------------------------------------------------------------------------------------------------------------
 
+def correct_intervals (records):
+    '''
+    correct_rr() will operate on the RR time series directly and will return another time series that can have a 
+    different timing (as the cumulative sum of the R interval will change). When we do not want this estimate to be 
+    contaminated by extreme RR intervals or even smaller deviations, those intervals are corrected by interpolation to 
+    make the time series as standard as possible, sacrificing the temporal precision of the heartbeat occurrence.
+
+    correct_peaks() will operate on the peaks vector directly. The number of peaks (and therefore the RR intervals) can 
+    vary, but the timing will remain constant. When the temporal precision of the heartbeat detection is relevant (this 
+    can concern heartbeat evoked potentials or instantaneous heart rate variability when it is time-locked to some 
+    specific stimuli. In this case, instead of blind interpolation, the raw signal time series can be used to 
+    re-estimate the peaks.
+    '''
+
+    # Simple clean for the most obvious artifacts
+    ibi = numpy.array (records['interbeat_intervals'], dtype = numpy.float64)
+    records['simple'] = [int (v) for v in ibi[ibi < 1800].tolist ()]
+    print ("Simple clean removed %d intervals." % (len (records['interbeat_intervals']) - len (records['simple'])))
+
+    # Recreate a new IBI time series that does not contain irregular intervals.
+    ibi, _ = systole.correction.correct_rr (ibi[ibi > 10])
+    ibi = [int (v) for v in ibi.tolist ()]
+    records['regular'] = ibi
+
+    # Better detection of R peaks
+    peaks = systole.correction.correct_peaks (ibi, input_type = "rr_ms", n_iterations = 2)
+    peaks = systole.utils.input_conversion (peaks['clean_peaks'], input_type = "peaks", output_type = "rr_ms")
+    records['peaks'] = [int (v) for v in peaks.tolist ()]
+
+    return records
+
+#-----------------------------------------------------------------------------------------------------------------------
+
 def extract_records (record_msgs):
     '''
     @return dict of lists per timestamp, interbeat_intervals and heart_rate
     '''
 
     # Actually, only the IBI is what is interesting
-    records = {"interbeat_intervals":[], "timestamp":[], "heart_rate":[]}
+    records = {"timestamp":[], "interbeat_intervals":[]}
 
     for msg in record_msgs:
         fields = msg['developer_fields']
@@ -97,6 +136,7 @@ def extract_records (record_msgs):
     duration_a = (max (records['timestamp']) - min (records['timestamp'])).total_seconds ()
     duration_b = sum (records['interbeat_intervals']) / 1000
     print ("Activity duration: %.1fs, Interval beats duration: %.1fs" % (duration_a, duration_b))
+    records.pop ('timestamp')
 
     # Type conversions (aka formatting in Spreadsheet applications)
     local_tz = get_localzone ()
@@ -104,7 +144,7 @@ def extract_records (record_msgs):
         for k in records.keys ():
             if k == 'timestamp':
                 records[k][i] = records[k][i].replace (tzinfo=pytz.utc).astimezone (local_tz)
-            elif k == 'interbeat_intervals' or k == 'heart_rate':
+            elif k == 'interbeat_intervals':
                 records[k][i] = int (records[k][i])
 
     return records
